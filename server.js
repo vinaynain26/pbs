f786a4ae3c8a2539d7352954edc971212fe4a07c
vinay.nain <vinay.nain@mastersunion.org>
2026-05-04 11:57:55 +0530

    added

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { engine } = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+
+app.engine("hbs", engine({
+  extname: ".hbs",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+  defaultLayout: false,
+  partialsDir: path.join(__dirname, "views/partials"),
+}));
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("pgptbm-index", {
+    layout: "pgptbm-index",
+    title: "PGPTBM Index",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
